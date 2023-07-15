5efdc4ace22b25c6b61adafb684eb78843de7b9a
tanmoy <tanmoy12deyashi@gmail.com>
2023-07-15 10:36:57 +0530

    added convertbytes to caches

--- src/app/components/caches/caches.component.ts
+++ src/app/components/caches/caches.component.ts
@@ -13,4 +13,16 @@ export class CachesComponent implements OnInit {
     console.log(this.cacheData)
   }
 
+  convertBytes(bytes: number): string {
+    if (bytes < 1024) {
+      return bytes + " B";
+    } else if (bytes < (1024 * 1024)) {
+      return (bytes / 1024).toFixed(2) + " KB";
+    } else if(bytes < (1024 * 1024 * 1024)) {
+      return (bytes / (1024 * 1024)).toFixed(2) + " MB";
+    } else {
+      return (bytes / (1024 * 1024 * 1024)).toFixed(2) + " GB";
+    }
+  }
+
 }
